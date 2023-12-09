835393222f12ea7e941c5ad3b5b69cfb1504216b
husseinhareb <shtam.hareb12@gmail.com>
2023-12-09 17:50:51 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.js'
+import Navbar from './Navbar.js';
+import Home from './Home.js'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='App'>
+      <Navbar/>
+      <div className='content'>
+        <Home />
+        <h1>App Component</h1>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>HomePage</h2>
+        </div>
+    )
+}
+
+export default Home;
