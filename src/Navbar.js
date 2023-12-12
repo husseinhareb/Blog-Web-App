4103a7c14a6ae00b85964f0cb53f56979a73ad66
husseinhareb <shtam.hareb12@gmail.com>
2023-12-12 18:26:44 +0200

    cleaning

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return(
         <nav className="navbar">
-            <h1>The Shtam Blog</h1>
+            <h1>The Everything Blog</h1>
             <div className="links">
             <Link to="/">Home</Link>
             <Link to="/create">New Blog</Link>
